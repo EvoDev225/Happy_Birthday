b888ee10ba45e47d8f30b639ae197b2f82cca1d3
Kambou oziel <ozielkambou602@gmail.com>
2026-08-19 12:09:29 +0000

    Ajout des variables

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -20,6 +20,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -36,7 +37,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                     ){
-                        GreetingImage(message = "Happy Birthday Sam!", from = "From Emma")
+                        GreetingImage(message = stringResource(R.string.happy_birthday_sam),
+                            from = stringResource(
+                            R.string.from_emma
+                        ))
                 }
             }
         }
@@ -75,7 +79,7 @@ fun GreetingText(message:String,from:String,modifier:Modifier = Modifier ){
             text = message,
             fontSize = 100.sp,
             lineHeight = 116.sp,
-            textAlign = TextAlign.Center
+            textAlign = TextAlign.Center,
         )
         Text(
             text = from,
@@ -87,10 +91,10 @@ fun GreetingText(message:String,from:String,modifier:Modifier = Modifier ){
     }
 }
 
-@Preview(showBackground = false)
 @Composable
 fun GreetingPreview() {
     HappyBirthdayTheme {
-        GreetingImage(message ="Happy Birthday Sam !",from="From Emma")
+        GreetingImage(message =stringResource(R.string.happy_birthday_sam),
+            from= stringResource(R.string.from_emma))
     }
 }
